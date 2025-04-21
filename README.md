6373797209e9aeb3d03905ff830a96de4dc8a087
promokk <andrey1803vorobyv@yandex.ru>
2025-04-21 23:09:43 +0300

    Добавил: - драфт pipeline-jmeter-dump-heap - readme

--- README.md
+++ README.md
@@ -1,2 +1,154 @@
-# pipeline-jmeter-dump-heap
-Pipeline для анализа JVM процессов Jmeter.
+# Pipeline-jmeter-dump-heap
+Jenkins pipeline для сбора диагностических данных памяти процессов Jmeter.  
+Pipeline подходит для любых java-процессов (JVM).  
+На выбранном сервере выполняются диагностические команды с помощью утилиты 
+[jcmd](https://www.baeldung.com/running-jvm-diagnose), после данные отправляются на сервер для анализа.  
+Возможности: снимок кучи, снимок потоков, снимок классов, выполнить диагностику утечки памяти.
+
+---
+# Оглавление
+Текст
+
+---
+## Начало работы <a id="getStart"></a>
+1. **Environment в pipeline.**  
+   Обязательные к редактированию:
+2. Обязательные properties, которые нужно отредактировать под ваш проект
+   * текст
+
+---
+## Описание pipeline <a id="pipelineDescription"></a>
+
+---
+### Этапы сборки <a id="assemblySteps"></a>
+Pipeline состоит из этапов (stage):
+* Deleting Workspace  
+  Содержит несколько шагов:
+    * DELETING WORKSPACE  
+      Рекурсивное удаление текущего каталога и его содержимого.
+    * CHECK SSH-KEYGEN  
+      Если во время сборки возникает ошибка "host key verification failed", то данный шаг решит эту проблему.
+* Info  
+  Вывод информации по процессам.
+* Create File  
+  Создание файлов для диагностики памяти.
+* Send File  
+  Содержит несколько шагов:
+    * DOWNLOAD FILE  
+      Загрузка архива на сервер Jenkins.
+    * SEND FILE  
+      Отправка архива на удаленный сервер.
+* finally  
+  Удаление временной директории.
+
+![assemblySteps - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/assemblySteps.png)
+
+---
+### Результат успешной сборки <a id="resultSuccessfulBuild"></a>
+Результат успешной сборки - архив с файлами.  
+Архив содержит:
+* Файл с информацие по процессам, info\_{SERVER}\_{timeNow}.txt.  
+  Данная информация небоходима для идентификации процессов.
+  ~~~
+  // Пример содержимого файла info_{SERVER}_{timeNow}.txt
+  // {pid}: {sun.java.command}
+  Process:
+	         1649: /opt/jmeter/bin/ApacheJMeter.jar -Dserver_port\=1099 -s -j jmeter-server.log
+	         8899: /opt/jmeter/bin/ApacheJMeter.jar -R localhost -n -t scriptExample.jmx
+  ~~~
+* Снимок кучи, heapDump\_{SERVER}\_{pid}\_{timeNow}.  
+  Файл будет создат при выполнении условия HEAPDUMP = true.
+* Снимок потоков, threadPrint\_{SERVER}\_{pid}\_{timeNow}.  
+  Файл будет создат при выполнении условия THREADPRINT = true.
+* Снимок классов, classHistogram\_{SERVER}\_{pid}\_{timeNow}.
+  Файл будет создат при выполнении условия CLASSHISTOGRAM = true.  
+* Снимок утилизации памяти, diagnoseMemory\_{SERVER}\_{pid}\_{timeNow}.  
+  Файл будет создат при выполнении условия DIAGNOSEMEMORY = true.  
+* Снимок утилизации памяти с разницей в утилизации памяти с течением времени, 
+  diagnoseMemoryLeak_${DIAGNOSETIME}\_{SERVER}\_{pid}\_{timeNow}.    
+  Файл будет создат при выполнении условия DIAGNOSEMEMORY = true и заполнен параметр DIAGNOSETIME.  
+  Данный файл создается вместо diagnoseMemory.
+
+!!! Мб добавить картинку пример вывода архива в виде дерева (если так можно) !!!  
+![XXX - картинка]()
+
+---
+### Используемые плагины Jenkins <a id="pluginsUseJenkins"></a>
+* Pipeline: Stage View Plugin
+* Rebuilder
+* SSH Agent Plugin
+* SSH Build Agents
+
+---
+## Параметры запуска <a id="launchOptions"></a>
+
+---
+### SERVER <a id="SERVER"></a>
+Сервер с котрого необходимо выболнить сбор диагностических данных памяти.  
+Максимальное кол-во серверов при запуске - 1.
+
+![SERVER - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/SERVER.png)
+
+---
+### HEAPDUMP <a id="HEAPDUMP"></a>
+Boolen значение, которое определяет, нужно ли выполнит снимок кучи (GC.heap_dump).
+* True - выполнить снимок кучи.
+* False - пропустить данный этап.
+
+![HEAPDUMP - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/HEAPDUMP.png)
+
+---
+### THREADPRINT <a id="THREADPRINT"></a>
+Boolen значение, которое определяет, нужно ли выполнит снимок потоков (Thread.print).
+* True - выполнить снимок потоков.
+* False - пропустить данный этап.
+
+![THREADPRINT - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/THREADPRINT.png)
+
+---
+### CLASSHISTOGRAM <a id="CLASSHISTOGRAM"></a>
+Boolen значение, которое определяет, нужно ли выполнит снимок классов (GC.class_histogram).
+* True - выполнить снимок классов.
+* False - пропустить данный этап.
+
+![CLASSHISTOGRAM - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/CLASSHISTOGRAM.png)
+
+---
+### DIAGNOSEMEMORY <a id="DIAGNOSEMEMORY"></a>
+Boolen значение, которое определяет, нужно ли выполнит снимок утилизации памяти или диагностику утечки памяти (VM.native_memory).
+* True - выполнить снимок утилизации памяти.  
+Дигностика утечки памяти выполняется, если указан параметр DIAGNOSETIME.
+* False - пропустить данный этап.
+
+![DIAGNOSEMEMORY - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/DIAGNOSEMEMORY.png)
+
+---
+### DIAGNOSETIME <a id="DIAGNOSETIME"></a>
+Продолжительность диагностики утечки памяти. Указывается в секундах.  
+Параметр заполняется, если небоходимо выполнить диагностику утечки памяти.  
+Параметр можно заполнить, если DIAGNOSEMEMORY = True. В иных случаях параметр будет игнорироваться.
+* Параметр заполнен.  
+Фиксируется базовый уровень утилизации памяти на момент выполнения команды (VM.native_memory baseline).  
+Через {DIAGNOSETIME} секунд выполняется команда для повторного снимка утилизации памяти (VM.native_memory summary.dif). 
+Это позволяет определить разницу в утилизации памяти с течением времени. Помогает выявить потенциальную утечку памяти.
+* Параметр не заполнен.  
+Выполнить снимок утилизации памяти (VM.native_memory).
+
+![DIAGNOSETIME - картинка](https://raw.githubusercontent.com/promokk/pipeline-jmeter-dump-heap/main/data/DIAGNOSETIME.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pipeline-jmeter-dump-heap.groovy
@@ -0,0 +1,162 @@
+node {
+    // environment
+    // Пользователь для подключения по ssh
+    def userSSH = 'andrey'
+    // Сredential для подключения по ssh
+    def credentialSSH = 'jenkins-ssh-key'
+    // Директория в которой будет создана временная директория для файлов (workDir)
+    def homeDir = '/home/andrey'
+    // Временная директория в которой будут создаваться файлы
+    def workDir = 'jmeterHeapDumps'
+    // Пользователь для подключения по ssh к удаленному серверу
+    def remoteUserSSH = 'andrey'
+    // Удаленный сервер на который будет отправлен архив с файлами
+    def remoteServer = '192.168.1.112'
+    // Директория на сервере {remoteServer} в которую будет отправлен архив с файлами
+    def remoteServerDirPath = '/home/andrey/jmeterHeapDumps'
+    // Уровень отслеживания памяти (summary / detail)
+    def memoryTrackingLevel = 'detail'
+    // Массив наименований созданных heapDump
+    def heapDumpArr = []
+    // Путь до workspace на сервере Jenkins
+    def workspacePath = sh(returnStdout: true, script: "pwd").trim()
+
+
+    try {
+        properties([
+            buildDiscarder(
+                logRotator(
+                    artifactDaysToKeepStr: '',
+                    artifactNumToKeepStr: '',
+                    daysToKeepStr: '14',
+                    numToKeepStr: '30')
+            ),
+            parameters([
+                string(
+                        name: 'SERVER',
+                        defaultValue: '192.168.1.111',
+                        description: 'Сервер',
+                        trim: true),
+                booleanParam(
+                        name: 'HEAPDUMP',
+                        defaultValue: true,
+                        description: 'Снимок кучи (GC.heap_dump)'),
+                booleanParam(
+                        name: 'THREADPRINT',
+                        defaultValue: false,
+                        description: 'Снимок потоков (Thread.print)'),
+                booleanParam(
+                        name: 'CLASSHISTOGRAM',
+                        defaultValue: false,
+                        description: 'Снимок классов (GC.class_histogram)'),
+                booleanParam(
+                        name: 'DIAGNOSEMEMORY',
+                        defaultValue: false,
+                        description: 'Снимок утилизации памяти (VM.native_memory)'),
+                string(
+                        name: 'DIAGNOSETIME',
+                        description: '''Продолжительность диагностики утечки памяти, сек.
+Параметр заполняется, если небоходимо выполнить диагности утечки памяти.
+Параметр можно заполнить, если DIAGNOSEMEMORY = True. В иных случаях параметр будет игнорироваться.''',
+                        trim: true)
+            ])])
+
+        // environment
+        // Время сейчас в миллисекундах
+        timeNow = System.currentTimeMillis()
+        // Массив процессов
+        try {
+            pidJmeter = sh(returnStdout: true, script: "ssh ${userSSH}@${SERVER} 'pgrep -f ApacheJMeter.jar'").trim().split('\n')
+        } catch(Exception e) {
+            error "Failed, process not found."
+        }
+
+        stage('Deleting Workspace') {
+            // Очистка WORKSPACE
+            echo '-----------------DELETING WORKSPACE-----------------'
+            deleteDir()
+
+            // Удаление и добавление ssh-ключа
+			echo '-----------------CHECK SSH-KEYGEN-----------------'
+            sh "ssh-keygen -R ${SERVER}"
+            sh "ssh-keyscan ${SERVER} >> ~/.ssh/known_hosts"
+        }
+        stage('Info') {
+            // Вывод информации по процессам
+            echo '-----------------INFO-----------------'
+            def messageInfo = "Process:"
+            for (pid in pidJmeter) {
+                def infoPid = sh(returnStdout: true, script: "ssh ${userSSH}@${SERVER} 'jcmd ${pid} VM.system_properties | grep sun.java.command'").trim().replace('sun.java.command=', "${pid}: ")
+                messageInfo += "\n\t${infoPid}"
+            }
+            echo messageInfo
+
+            currentBuild.displayName = "${env.BUILD_ID}--${SERVER}"
+            currentBuild.description = "${messageInfo}"
+
+            // Создание файла с информацией по процессам
+            sh """ssh ${userSSH}@${SERVER} '''rm -r ${homeDir}/${workDir}; mkdir ${homeDir}/${workDir}
+cat << EOF > ${homeDir}/${workDir}/info_${SERVER}_${timeNow}.txt
+${messageInfo}
+EOF
+'''"""
+        }
+        stage('Create File') {
+            sshagent(["${credentialSSH}"]) {
+                // Создание файлов для диагностики памяти
+                echo '-----------------CREATE FILE-----------------'
+                for (pid in pidJmeter) {
+                    if (params.DIAGNOSEMEMORY) {
+                        if (params.DIAGNOSETIME) {
+                            echo 'Create Baseline Diagnose Memory Leak...'
+                            sh "ssh ${userSSH}@${SERVER} 'jcmd ${pid} VM.native_memory baseline'"
+                        } else {
+                            echo 'Create Diagnose Memory...'
+                            sh "ssh ${userSSH}@${SERVER} 'jcmd ${pid} VM.native_memory > ${homeDir}/${workDir}/diagnoseMemory_${SERVER}_${pid}_${timeNow}'"
+                        }
+                        if (params.DIAGNOSETIME) {
+                            echo "Continued in ${DIAGNOSETIME} seconds..."
+                            sleep params.DIAGNOSETIME
+                            echo 'Create Difference To Diagnose Memory Leak...'
+                            sh "ssh ${userSSH}@${SERVER} 'jcmd ${pid} VM.native_memory ${memoryTrackingLevel}.diff > ${homeDir}/${workDir}/diagnoseMemoryLeak_${DIAGNOSETIME}_${SERVER}_${pid}_${timeNow}'"
+                        }
+                    }
+                    if (params.HEAPDUMP) {
+                        echo 'Create Heap Dump...'
+                        sh "ssh ${userSSH}@${SERVER} 'jcmd ${pid} GC.heap_dump ${homeDir}/${workDir}/heapDump_${SERVER}_${pid}_${timeNow}'"
+                    }
+                    if (params.THREADPRINT) {
+                        echo 'Create Thread Print...'
+                        sh "ssh ${userSSH}@${SERVER} 'jcmd ${pid} Thread.print > ${homeDir}/${workDir}/threadPrint_${SERVER}_${pid}_${timeNow}'"
+                    }
+                    if (params.CLASSHISTOGRAM) {
+                        echo 'Create Class Histogram...'
+                        sh "ssh ${userSSH}@${SERVER} 'jcmd ${pid} GC.class_histogram > ${homeDir}/${workDir}/classHistogram_${SERVER}_${pid}_${timeNow}'"
+                    }
+                }
+                fileStr = sh(returnStdout: true, script: "ssh ${userSSH}@${SERVER} 'ls ${homeDir}/${workDir}'").trim().replaceAll('\\s', ' ')
+                sh "ssh ${userSSH}@${SERVER} 'cd ${homeDir}/${workDir}; zip memoryUsage_${SERVER}_${timeNow}.zip ${fileStr}'"
+            }
+        }
+        stage('Send File') {
+            sshagent(["${credentialSSH}"]) {
+                // Загрузка архива на сервер Jenkins
+                echo '-----------------DOWNLOAD FILE-----------------'
+                archiveFile = sh(returnStdout: true, script: "ssh ${userSSH}@${SERVER} 'ls ${homeDir}/${workDir} | grep memoryUsage'").trim()
+                sh "scp ${userSSH}@${SERVER}:${homeDir}/${workDir}/${archiveFile} ${workspacePath}"
+
+                // Отправка архива на удаленный сервер
+                echo '-----------------SEND FILE-----------------'
+                sh "scp ${archiveFile} ${remoteUserSSH}@${remoteServer}:${remoteServerDirPath}"
+            }
+        }
+    }  catch (e) {
+        echo "Failed because of {$e}"
+    } finally {
+        sshagent(["${credentialSSH}"]) {
+            // Удаление временной директории
+            echo '-----------------END-----------------'
+            sh "ssh ${userSSH}@${SERVER} 'rm -r ${homeDir}/${workDir}'"
+        }
+    }
+}
